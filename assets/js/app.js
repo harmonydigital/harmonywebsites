ea526cff1455f733f420305b6f2f54d17eb1b8b7
harmonydigital <131012395+harmonydigital@users.noreply.github.com>
2023-08-20 14:26:39 -0300

    v1.0

--- assets/js/app.js
+++ assets/js/app.js
@@ -22,7 +22,7 @@ initapp=()=>{
                             </a>
                         </li>
                         <li>
-                            <a class='btn inline' href="">
+                            <a class='btn inline' href="https://github.com/harmonydigital/harmonywebsites" target='_blank'>
                                 <span>v1.0</span>
                                 <i class="fa-brands fa-github"></i>
                             </a>
@@ -36,44 +36,46 @@ initapp=()=>{
         <main> 
                 <div class='display'> 
                         <div class='content-text'>
-                            <span>Harmony</span>
+                            <span class='title'>Harmony</span>
                             <h1>Visível, leve e intuitivo.</h1>
                             <h2>ative a criatividade e comece algo novo  agora mesmo...</h2>
+                            <a class='btn-down' href=''>
+                                <i class="fa-solid fa-chevron-down"></i>
+                            </a>
                         </div>
                         <div class='social'>
-                            <a class='' href="">
-                                
-                            <i class="fa-brands fa-instagram"></i>
+                            <a class='' href="">    
+                                <i class="fa-brands fa-instagram"></i>
                             </a>
-                            <a class='' href="">
-                               
-                            <i class="fa-brands fa-facebook"></i>
+                            
+                            <a class='' href="https://www.linkedin.com/in/harmony-digital/" target="_blank">
+                              <i class="fa-brands fa-linkedin-in"></i>
                             </a>
-                            <a class='' href="">
-                            <i class="fa-brands fa-linkedin-in"></i>
-                            </a>
-                            <a class='' href="">
-                                
+                            <a class='' href="https://github.com/harmonydigital" target="_blank" > 
                                 <i class="fa-brands fa-github"></i>
                             </a>
                         </div>
+
+
                 </div>
 
                 <div class='harmonyabout'>
 
                     <div class='content-text'>
+                       <span class='title'></span>
                        <h2>produtos
                        para pessoas
-                       como eu e você</h2>
+                       como eu e você.</h2>
                     </div>
+
                     <div>
-                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-                 </div>
-                
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</br></br> Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </br></br>It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                    </div>
+                  
                 </div>
 
                 <div class='slide'>
-                    <!-- Swiper -->
+                    <!-- Swipesr -->
                     <div class="swiper mySwiper">
                     <div class="swiper-wrapper">
                         <div class="swiper-slide"><img class='logo' src='assets/img/modelo.png'></div>
