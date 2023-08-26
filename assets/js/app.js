07e760ebc6e184f98558c800b79ab9d695d204cd
harmonydigital <131012395+harmonydigital@users.noreply.github.com>
2023-08-26 10:41:55 -0300

    Add files via upload

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,11 +13,11 @@ initapp=()=>{
                         </li>
                        
                         <li>
-                            <a  href='#about'>Sobre</a>
+                            <a href='#about'>Sobre</a>
                         </li>
                         <li>
                             <a class='btn primary convert-btn' href="">
-                                <span>Fale agora</span>
+                                <span>Fale comigo</span>
                                 <i class="fa-brands fa-whatsapp"></i>
                             </a>
                         </li>
@@ -66,14 +66,20 @@ initapp=()=>{
                        <h2>produtos
                        para pessoas
                        como eu e você.</h2>
-                       <a class='btn primary convert-btn' href="">
-                       <span>Fale agora</span>
+                       <a class='anchor btn primary convert-btn h-xs' href='#about'>
+                       <span>Fale comigo</span>
                             <i class="fa-brands fa-whatsapp"></i>
                         </a>
                     </div>
 
                     <div>
-                        <p>Harmony flow é se traduz na relação produto / cliente.</p>
+                        <p>Harmony se traduz na relação mais que eficiente, entre produto e pessoas.
+                        </br></br>Somos a estreita relação do homem e tecnologia.
+                        Amamos criar soluções reais, e tornar a vida de pessoas ainda melhor através da tecnologia.
+                        Somos um grupo de profissionais do design.
+                        Nós nos unimos, para falar sobre design, projetar novos modelos e testar novas soluções.
+                        </br></br>Aqui tudo começa na relação de pessoa para pessoa, assim criamos um conexão com o interlocutor, onde podemos entender como o design pode solucionar ou otimizar sua vida.
+                        Veja o que fizemos até aqui, e crie algo novo você também..</p>
                     </div>
                   
                 </div>
